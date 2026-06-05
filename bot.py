4ea4f78eea26070e0c54f9359e5c26826d9b50a8
Brady Kelly <brady_kelly@tutamail.com>
2026-06-05 07:29:00 +0200

    newchannel command

--- bot.py
+++ bot.py
@@ -31,5 +31,19 @@ async def nine_nine(ctx):
 @bot.command(name='roll', help='Simulates rolling dice.')
 async def roll(ctx, number_of_dice: int, number_of_sides: int):
     await comms.roll(ctx, number_of_dice, number_of_sides)
+    
+@bot.command(name='channel-new')
+@commands.has_role('admin')
+async def create_channel(ctx, channel_name='newchan'):
+    guild = ctx.guild
+    existing_channel = discord.utils.get(guild.channels, name=channel_name)
+    if not existing_channel:
+        print(f'Creating a new channel: {channel_name}')
+        await guild.create_text_channel(channel_name)    
+        
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.errors.CheckFailure):
+        await ctx.send('You do not have the correct role for this command.')        
 
 bot.run(TOKEN)
