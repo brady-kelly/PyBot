7b61ec8e22a23d0bb48faadba36c81ecf4c90937
Brady Kelly <brady_kelly@tutamail.com>
2026-06-06 10:16:07 +0200

    Check

--- cogs/super_cog.py
+++ cogs/super_cog.py
@@ -17,6 +17,23 @@ class SuperCog(commands.Cog):
         if message.author == self.bot.user:
             return
         self.logger.info(f"message from {message.author}: {message.content}")        
+        
+    @commands.hybrid_group(fallback="foo")
+    async def foo(self, ctx: commands.Context):
+        """This is a hybrid command group."""
+        await ctx.send("foo")
+
+    # /foo bar | !foo bar
+    @foo.command()
+    async def bar(self, ctx: commands.Context):
+        """This is a subcommand."""
+        await ctx.send("bar")
+
+    # /foo baz | !foo baz
+    @foo.command()
+    async def baz(self, ctx: commands.Context):
+        """This is a regular command."""
+        await ctx.send("baz")        
 
 async def setup(bot):
     await bot.add_cog(SuperCog(bot))
